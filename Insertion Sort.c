c4803397d8cbb0dd191b6865fcecd1c18983bc90
Sk Sakib <109467015+sksaakeeb@users.noreply.github.com>
2022-11-26 15:48:35 +0530

    Add files via upload

---
+++ Insertion Sort.c
@@ -0,0 +1,35 @@
+//Insertion Sort 
+
+#include <stdio.h>
+
+int main(void)
+{
+    int arr[50], n, i, j, temp;
+    printf("Enter number of elements\n");
+    scanf("%d", &n);
+
+    printf("Enter %d integers\n", n);
+    for (i = 0; i < n; i++) 
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    for (i = 1; i < n; i++) 
+    {
+        j = i;
+        while (j > 0 && arr[j - 1] > arr[j]) 
+        {
+            temp = arr[j];
+            arr[j] = arr[j - 1];
+            arr[j - 1] = temp;
+            j--;
+        }
+    }
+
+    printf("Sorted list in ascending order:\n");
+    for (i = 0; i < n; i++) 
+    {
+        printf("%d\n", arr[i]);
+    }
+    return 0;
+}
