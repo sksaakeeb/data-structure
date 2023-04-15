eff9fd1bdae2121dbef5d4da1a09496060547eb2
Sk Sakib <109467015+sksaakeeb@users.noreply.github.com>
2023-04-15 12:49:15 +0530

    Add files via upload

---
+++ binary_tree/postOrder_traversal.c
@@ -0,0 +1,59 @@
+// Binary Tree - PostOrder Traversal
+
+#include <stdio.h>
+#include <malloc.h>
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+
+struct Node *createNode(int data)
+{
+    struct Node *n;                                 // Creating a node pointer
+    n = (struct Node *)malloc(sizeof(struct Node)); // Allocating memory in the heap
+    n->data = data;                                 // Assign data
+    n->left = NULL;                                 // Assign left child to NULL
+    n->right = NULL;                                // Assign right child to NULL
+    return n;                                       // Returning the created node
+}
+
+void postOrder(struct Node *root)
+{
+    if (root != NULL)
+    {
+        postOrder(root->left);
+        postOrder(root->right);
+        printf("%d ", root->data);
+    }
+}
+
+int main()
+{
+
+    // Constracting the nodes
+    struct Node *p = createNode(4);
+    struct Node *p1 = createNode(1);
+    struct Node *p2 = createNode(6);
+    struct Node *p3 = createNode(5);
+    struct Node *p4 = createNode(2);
+
+    // Tree looks:
+
+    //          4
+    //         / \
+    //        1   6
+    //       / \
+    //      5   2
+    //
+
+    // Linking the root node with right and left child
+    p->left = p1;
+    p->right = p2;
+    p1->left = p3;
+    p1->right = p4;
+
+    postOrder(p);
+}
